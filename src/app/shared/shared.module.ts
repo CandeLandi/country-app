2da69fc2f1b3ef34edb53eae7c41991293c6f0ee
Candela Landi <landicandela01@gmail.com>
2024-03-14 10:11:46 -0300

    create countries pages

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    SidebarComponent,
+    AboutPageComponent,
+    ContactPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HomePageComponent,
+    SidebarComponent,
+    AboutPageComponent,
+    ContactPageComponent
   ]
 })
 export class SharedModule { }
